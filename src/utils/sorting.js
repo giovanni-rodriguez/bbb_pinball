8459cff80e88b2aeb1acd29ddfe1a8eab3cbd943
giovanni-rodriguez <89277776+giovanni-rodriguez@users.noreply.github.com>
2023-01-29 21:30:22 -0500

    Updated sorting to a more efficient sorting algorithm(mergeSort)

--- src/utils/sorting.js
+++ src/utils/sorting.js
@@ -1,14 +1,91 @@
-const sortLocations = (locations, sortBy, sortOrder) => {
-    if (sortBy === "Distance") {
-        locations = locations.sort((a, b) => sortOrder === "Ascending" ? a.distance - b.distance : b.distance - a.distance);
+const merge = (left, right, sortBy, sortOrder) => {
+    let result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (sortBy === "Distance") {
+            if (sortOrder === "Ascending") {
+                if (left[i].distance < right[j].distance) {
+                    result.push(left[i]);
+                    i++
+                } else {
+                    result.push(right[j]);
+                    j++
+                }
+            } else {
+                if (left[i].distance > right[j].distance) {
+                    result.push(left[i]);
+                    i++
+                }
+                else {
+                    result.push(right[j]);
+                    j++
+                }
+            }
+        } else if (sortBy === "Name") {
+            if (sortOrder === "Ascending") {
+                if (left[i].name.localeCompare(right[j].name) < 0) {
+                    result.push(left[i]);
+                    i++
+                } else {
+                    result.push(right[j]);
+                    j++
+                }
+            } else {
+                if (left[i].name.localeCompare(right[j].name) > 0) {
+                    result.push(left[i]);
+                    i++
+                } else {
+                    result.push(right[j]);
+                    j++
+                }
+            }
+        } else if (sortBy === "Number of machines") {
+            if (sortOrder === "Ascending") {
+                if (left[i].num_machines < right[j].num_machines) {
+                    result.push(left[i]);
+                    i++
+                } else {
+                    result.push(right[j]);
+                    j++
+                }
+            } else {
+                if (left[i].num_machines > right[j.num_machines]) {
+                    result.push(left[i]);
+                    i++
+                } else {
+                    result.push(right[j]);
+                    j++
+                }
+            }
+        }
     }
-    else if (sortBy === "Name") {
-        locations = locations.sort((a, b) => sortOrder === "Ascending" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name));
+    while (i < left.length) {
+        result.push(left[i]);
+        i++
     }
-    else if (sortBy === "Number of machines") {
-        locations = locations.sort((a, b) => sortOrder === "Ascending" ? a.num_machines - b.num_machines : b.num_machines - a.num_machines);
+    while (j < right.length) {
+        result.push(right[j]);
+        j++
     }
-    return locations
+    return result;
 }
 
-export default sortLocations
+const mergeSort = (locations, sortBy, sortOrder) => {
+    if (locations.length < 2) {
+        return locations;
+    }
+
+    const middle = Math.floor(locations.length / 2);
+    const left = locations.slice(0, middle);
+    const right = locations.slice(middle);
+
+    return merge(mergeSort(left, sortBy, sortOrder), mergeSort(right, sortBy, sortOrder), sortBy, sortOrder);
+}
+
+const sortLocations = (locations, sortBy, sortOrder) => {
+    return mergeSort(locations, sortBy, sortOrder)
+}
+
+export default sortLocations;
